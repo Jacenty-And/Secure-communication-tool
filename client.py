04579b769fbda9387aaf9bdef30e68f96441680d
jacenty <jacentyand@gmail.com>
2023-04-28 15:21:12 +0200

    Version 1.0 Client is hosting the connection

--- client.py
+++ client.py
@@ -1,41 +1,56 @@
 import socket
-from encryption import encrypt
+from encryption import encrypt, decrypt
 from threading import Thread
 from queue import Queue
 
 
-class Client(Thread):
-    def __init__(self, host, port, public_key):
-        super().__init__(daemon=True)
-        self.host = host
-        self.port = port
+class Client:
+    def __init__(self, host, port, private_key, public_key):
+        self.private_key = private_key
         self.public_key = public_key
         self.messages_to_send = Queue()
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connected = False
+        self.messages_received = Queue()
+        self.running = False
+        try:
+            # Client is hosting the connection if possible
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+                server_socket.bind((host, port))
+                print("I'm hosting!")
+                server_socket.listen()
+                self.client_socket, address = server_socket.accept()
+        except:
+            # If there is already a host, client is connecting to it
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((host, port))
+            print("I connected to the host!")
 
     def add_message(self, message) -> None:
         self.messages_to_send.put(message)
 
-    def connect(self) -> bool:
-        try:
-            self.socket.connect((self.host, self.port))
-        except ConnectionRefusedError:
-            return False
-        self.connected = True
-        return True
+    def get_messages(self) -> list:
+        messages = list()
+        while not self.messages_received.empty():
+            messages.append(self.messages_received.get())
+        return messages
 
-    def send(self, message) -> bool:
-        ciphertext = encrypt(message, self.public_key)
-        self.socket.sendall(ciphertext)
-        return True
-
-    def run(self) -> None:
+    def send_threading(self) -> None:
         while True:
             message = self.messages_to_send.get()
-            sent = False
-            while not sent:
-                if not self.connected:
-                    self.connect()
-                else:
-                    sent = self.send(message)
+            ciphertext = encrypt(message, self.public_key)
+            self.client_socket.send(ciphertext)
+
+    # TODO ConnectionResetError: [WinError 10054] An existing connection was forcibly closed by the remote host
+    def receive_threading(self) -> None:
+        while True:
+            message = self.client_socket.recv(1024)
+            decrypted = decrypt(message, self.private_key)
+            self.messages_received.put(decrypted)
+
+    def run(self) -> None:
+        if not self.running:
+            Thread(target=self.send_threading, daemon=True).start()
+            Thread(target=self.receive_threading, daemon=True).start()
+            self.running = True
+        else:
+            print("Client is already running")
+
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from encryption import load_keys
-from server import Server
 from client import Client
 
 HOST = "127.0.0.1"
@@ -7,12 +6,8 @@ PORT = 65432
 
 if __name__ == '__main__':
     private_key, public_key = load_keys()
-    port_client = int(input("Enter port to which messages will be sent: "))
-    client = Client(HOST, port_client, public_key)
-    client.start()
-    port_server = int(input("Enter port from which messages will be received: "))
-    server = Server(HOST, port_server, private_key)
-    server.start()
+    client = Client(HOST, PORT, private_key, public_key)
+    client.run()
     while True:
         print("1. Send")
         print("2. Receive")
@@ -22,7 +17,7 @@ if __name__ == '__main__':
             message = input("Message: ")
             client.add_message(message)
         elif menu == "2":
-            for message in server.get_messages():
+            for message in client.get_messages():
                 print(message)
         elif menu == "3":
             exit()
