5f7ef6df18b46a3e9d476b528b01aa855e243826
jacenty <jacentyand@gmail.com>
2023-04-30 14:17:44 +0200

    Version 1.1 Client starts hosting a connection when the previous host disconnects

--- client.py
+++ client.py
@@ -1,28 +1,44 @@
 import socket
 from encryption import encrypt, decrypt
-from threading import Thread
+from threading import Thread, Event
 from queue import Queue
 
 
 class Client:
     def __init__(self, host, port, private_key, public_key):
+        self.host_num = host
+        self.port_num = port
         self.private_key = private_key
         self.public_key = public_key
         self.messages_to_send = Queue()
         self.messages_received = Queue()
+        self.client_socket = self.try_host_else_connect()
         self.running = False
+
+    def try_host_else_connect(self) -> socket.socket:
         try:
             # Client is hosting the connection if possible
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-                server_socket.bind((host, port))
-                print("I'm hosting!")
-                server_socket.listen()
-                self.client_socket, address = server_socket.accept()
-        except:
+            client_socket = self.host()
+        except WindowsError:
             # If there is already a host, client is connecting to it
-            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client_socket.connect((host, port))
-            print("I connected to the host!")
+            client_socket = self.connect()
+        return client_socket
+
+    def host(self) -> socket.socket:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((self.host_num, self.port_num))
+            print("I'm hosting!")
+            server_socket.listen()
+            print("Waiting for someone to connect...")
+            client_socket, address = server_socket.accept()
+            print(f"{address} connected!")
+        return client_socket
+
+    def connect(self) -> socket.socket:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((self.host_num, self.port_num))
+        print("I connected to the host!")
+        return client_socket
 
     def add_message(self, message) -> None:
         self.messages_to_send.put(message)
@@ -39,12 +55,17 @@ class Client:
             ciphertext = encrypt(message, self.public_key)
             self.client_socket.send(ciphertext)
 
-    # TODO ConnectionResetError: [WinError 10054] An existing connection was forcibly closed by the remote host
     def receive_threading(self) -> None:
         while True:
-            message = self.client_socket.recv(1024)
-            decrypted = decrypt(message, self.private_key)
-            self.messages_received.put(decrypted)
+            try:
+                # Client is trying to receive a message
+                message = self.client_socket.recv(1024)
+                decrypted = decrypt(message, self.private_key)
+                self.messages_received.put(decrypted)
+            except WindowsError:
+                # If the existing connection is closed, client become a host
+                print("Connection lost!")
+                self.client_socket = self.try_host_else_connect()
 
     def run(self) -> None:
         if not self.running:
@@ -52,5 +73,5 @@ class Client:
             Thread(target=self.receive_threading, daemon=True).start()
             self.running = True
         else:
-            print("Client is already running")
+            raise Exception("Client can't be run multiple times")
 
