d940d78fa9a321096fb0223dc61cbb27a92daff3
jacenty <jacentyand@gmail.com>
2023-05-16 00:52:54 +0200

    AES encryption module

---
+++ aes_encryption.py
@@ -0,0 +1,36 @@
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+
+def generate_random_key(size=32):
+    return get_random_bytes(size)
+
+
+def encrypt(message: str, key, mode="ECB"):
+    if mode == "ECB":
+        cipher = AES.new(key, AES.MODE_ECB)
+        return cipher.encrypt(pad(message.encode(), AES.block_size))
+    elif mode == "CBC":
+        cipher = AES.new(key, AES.MODE_CBC)
+        return cipher.encrypt(pad(message.encode(), AES.block_size)), cipher.iv
+    else:
+        raise Exception("Mode not implemented!")
+
+
+def decrypt(data: bytes, key, mode="ECB", iv=None):
+    if mode == "ECB":
+        cipher = AES.new(key, AES.MODE_ECB)
+    elif mode == "CBC":
+        cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    else:
+        raise Exception("Mode not implemented!")
+    return unpad(cipher.decrypt(data), AES.block_size).decode()
+
+
+# key = generate_random_key()
+# message = input("Message: ")
+# ciphered = encrypt(message, key, mode="ECB")
+# print(ciphered)
+# decrypted = decrypt(ciphered, key, mode="ECB")
+# print(decrypted)
