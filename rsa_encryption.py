ce84e1c933cd98d66d88506f038244f6cb88e54a
jacenty <jacentyand@gmail.com>
2023-06-03 00:48:36 +0200

    Library to handle RSA encryption changed

--- rsa_encryption.py
+++ rsa_encryption.py
@@ -6,6 +6,40 @@ from rsa import decrypt as rsa_decrypt
 
 from aes_encryption import encrypt_CBC
 
+from Crypto.PublicKey import RSA
+from Crypto.Cipher.PKCS1_OAEP import
+
+
+def generate_keys() -> Tuple[RSA.RsaKey, RSA.RsaKey]:
+    private_key = RSA.generate(1024)
+    public_key = private_key.public_key()
+    return public_key, private_key
+
+
+def save_public_key(public_key: RSA.RsaKey, path: str) -> None:
+    with open(f"{path}/public_key.pem", "wb") as file:
+        file.write(public_key.export_key("PEM"))
+
+
+def save_private_key(private_key: RSA.RsaKey, path: str, password: str) -> None:
+    with open(f"{path}/private_key.pem", "wb") as file:
+        file.write(private_key.export_key("PEM",
+                                          passphrase=password,
+                                          protection="PBKDF2WithHMAC-SHA1AndDES-EDE3-CBC",
+                                          pkcs=8))
+
+
+def load_public_key(path: str) -> RSA.RsaKey:
+    with open(f"{path}/public_key.pem", "rb") as file:
+        public_key = RSA.import_key(file.read())
+    return public_key
+
+
+def load_private_key(path: str, password: str) -> RSA.RsaKey:
+    with open(f"{path}/private_key.pem", "rb") as file:
+        private_key = RSA.import_key(file.read(), passphrase=password)
+    return private_key
+
 
 def generate_keys() -> Tuple[PublicKey, PrivateKey]:
     public_key, private_key = newkeys(1024)
